4615df1b08d8cacaf4bf6fad10eb91f9dd83b7fd
gamal2001 <kmhamak@gmail.com>
2023-09-19 18:41:06 +0300

    creat the navbar and hero components

---
+++ airbnb-experiences-clone/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import image from "../assets/Group 77.png";
+function Hero() {
+  return (
+    <section className="hero">
+      <img src={image} alt="image" className="hero--photo" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ airbnb-experiences-clone/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/airbnb-logo.svg"
+
+function Navbar() {
+  return(
+    <nav>
+        <img src={logo} alt="" />
+    </nav>
+  );
+}
+
+export default Navbar;
