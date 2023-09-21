446c396ef5b6dce7e04b03d80a9ffb65e23cc115
gamal2001 <kmhamak@gmail.com>
2023-09-22 02:33:22 +0300

    creat the card component

--- airbnb-experiences-clone/src/App.jsx
+++ airbnb-experiences-clone/src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </>
   );
 }
