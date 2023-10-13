9e5f042d6996d119ed1c3ea5af044f5472f92835
gamal2001 <kmhamak@gmail.com>
2023-10-13 04:35:47 +0300

    Die component

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,5 +1,20 @@
 import "./App.css";
-
+import Die from "./components/Die";
 export default function App() {
-  return <main>something</main>;
+  return (
+    <main>
+      <div className="dies-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="8" />
+        <Die value="9" />
+        <Die value="10" />
+      </div>
+    </main>
+  );
 }
