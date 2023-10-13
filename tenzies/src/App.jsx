a7a29592568f971fa11eb3f541d4db90d0b4a5b6
gamal2001 <kmhamak@gmail.com>
2023-10-13 07:02:13 +0300

    change dice to objects

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,22 +1,29 @@
 import "./App.css";
 import Die from "./components/Die";
 import { useState } from "react";
+import { nanoid } from 'nanoid'
+
 export default function App() {
   const [dice, setDice] = useState(allNewDice())
   function rollDice(){
     setDice(allNewDice())
   }
-  function allNewDice(){
-    let newDice = []
-    for(let i = 0; i < 10; i++){
-      let randomNumber = Math.floor(Math.random() * 7)
-      newDice.push(randomNumber)
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Object.values({
+          value: Math.ceil(Math.random() * 6), 
+            isHeld: false,
+        }))
     }
     return newDice
-  }
+}
+
 const diceElements = dice.map(( die, index) => {
   return <Die key={index} value = {die}/>
 })
+
   return (
     <main>
       <div className="dies-container">
