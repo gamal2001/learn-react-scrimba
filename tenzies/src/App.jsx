5b7b1ca1dc6490ed4a21636561bc24809a8617c5
gamal2001 <kmhamak@gmail.com>
2023-10-13 06:04:15 +0300

    roll dice button

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import Die from "./components/Die";
+import { useState } from "react";
 export default function App() {
+  const [dice, setDice] = useState(allNewDice())
+  function rollDice(){
+    setDice(allNewDice())
+  }
   function allNewDice(){
     let newDice = []
     for(let i = 0; i < 10; i++){
@@ -9,21 +14,15 @@ export default function App() {
     }
     return newDice
   }
-console.log(allNewDice())
+const diceElements = dice.map(( die, index) => {
+  return <Die key={index} value = {die}/>
+})
   return (
     <main>
       <div className="dies-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="10" />
+        {diceElements}
       </div>
+      <button className="roll--dice" onClick={rollDice}>Roll</button>
     </main>
   );
 }
